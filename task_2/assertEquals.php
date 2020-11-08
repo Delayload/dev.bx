c1159d15cf01618c1db4d374560be88119c1077e
delayload <alexjaworski88@gmail.com>
2020-11-08 21:39:03 +0200

    task_2 - поправил assertEquals

--- task_2/assertEquals.php
+++ task_2/assertEquals.php
@@ -2,9 +2,9 @@
 
 function assertEquals($expectedValue, string $result,string $inputValue)
 {
-	$testResult = $result===$expectedValue? 'passed': 'failed';
+	$testResult = ($result===$expectedValue)? 'passed': 'failed';
 
-	echo $result. ' ' .is_numeric($result).PHP_EOL;
-	echo $expectedValue. ' ' .is_numeric($expectedValue).PHP_EOL;
+	echo '$result = '.$result. ' .is_numeric = ' .is_numeric($result).PHP_EOL;
+	echo '$expectedValue = '.$expectedValue. ' .is_numeric = ' .is_numeric($expectedValue).PHP_EOL;
 	echo $inputValue.' '.$testResult;
 }
