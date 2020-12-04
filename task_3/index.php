7387f24080f4c95dc93e8f005d28057acb7a5b0f
delayload <alexjaworski88@gmail.com>
2020-12-04 05:41:39 +0200

    исправление html

--- task_3/index.php
+++ task_3/index.php
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+<?php
+
+
+header('Content-Type: text/html; charset=UTF-8');
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_regex_encoding('UTF-8');
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
