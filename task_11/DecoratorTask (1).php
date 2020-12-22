6f83a73f5c807b60dd7510a7de2382f21ecd5d79
delayload <alexjaworski88@gmail.com>
2020-12-22 22:20:13 +0200

    added Decorator

--- task_11/DecoratorTask (1).php
+++ task_11/DecoratorTask (1).php
@@ -1,21 +1,68 @@
 <?php
 
-class Square
+interface Shape
 {
-	public function draw(): void
+	public function draw() : string;
+}
+
+class Square implements Shape
+{
+	public function draw() : string
 	{
-		echo "Shape Square\n";
+		return "Shape Square\n";
 	}
 }
 
-class Circle
+class Circle implements Shape
 {
-	public function draw(): void
+	public function draw() : string
 	{
-		echo "Shape Circle\n";
+		return "Shape Circle\n";
 	}
 }
 
+abstract class ColorDecorator implements Shape
+{
+	protected $shape;
+
+	public function __constructor(Shape $shape)
+	{
+		$this->shape = $shape;
+	}
+
+	public function draw() : string
+	{
+		return $this->shape->draw();
+	}
+}
+
+class RedColor extends ColorDecorator
+{
+	public function draw(): string
+	{
+		return "Red colored ".parent::draw();
+	}
+}
+
+class clientCode
+{
+	protected $shape;
+
+	public function __construct(Shape $shape)
+	{
+		$this->shape = $shape;
+	}
+
+	public function drawShape() : string
+	{
+		return $this->shape->draw();
+	}
+}
+
+$square = new Square();
+$redShape = new RedColor($square);
+$shapeUser = new clientCode($redShape);
+$shapeUser->drawShape();
 /*
 	Необходимо воспользоваться шаблоном проектирования "Декоратор" для того, чтобы иметь возможность
 	"получать" в итоге фигуры разных цветов, например вызов декоратора:
