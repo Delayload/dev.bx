c9ff059515278de9da5493bc7b0f448d2d037193
delayload <alexjaworski88@gmail.com>
2021-01-01 19:06:48 +0200

    added table 'teams'

---
+++ task_12(tournament)/db_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS team
+(
+	ID int not null auto_increment,
+	NAME varchar(500) not null,
+	PRIMARY KEY (ID)
+);
---
+++ task_12(tournament)/db_create_data.sql
@@ -0,0 +1,17 @@
+INSERT IGNORE INTO team (ID, NAME)
+VALUES (1, 'Uruguay'),
+       (2, 'Portugal'),
+       (3, 'France'),
+       (4, 'Argentina'),
+       (5, 'Brazil'),
+       (6, 'Mexico'),
+       (7, 'Belgium'),
+       (8, 'Japan'),
+       (9, 'Spain'),
+       (10, 'Russia'),
+       (11, 'Croatia'),
+       (12, 'Denmark'),
+       (13, 'Sweden'),
+       (14, 'Switzerland'),
+       (15, 'Colombia'),
+       (16, 'England');
