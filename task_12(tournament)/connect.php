3a81a626b48e69df2ea231f3171ddbfcb08e65c9
Sergey Volkov <volkov.sergey.delayload@gmail.com>
2021-01-02 13:22:24 +0200

    Delete connect.php

--- task_12(tournament)/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connection = mysqli_init();
-$host = 'localhost';
-$username = 'student';
-$password = 'student';
-$dbName = 'tournament';
-$connectionResult = $connection->real_connect($host, $username, $password, $dbName);
