d706434b50cbef52f698df3a9e85c14f4f049150
delayload <alexjaworski88@gmail.com>
2021-01-14 09:03:00 +0200

    получение данных с бд

--- connect.php
+++ connect.php
@@ -10,23 +10,14 @@ if (!$connectionResult)
 	trigger_error($error, E_USER_ERROR);
 }
 
-$ID = "1";
-$ID = (int)$ID;
-
-$query = "SELECT * FROM team WHERE ID = $ID";
+$query = "SELECT t.NAME, b.NAME, FIRST_TEAM_SCORE, SECOND_TEAM_SCORE, s.NAME
+FROM game
+	     JOIN team t ON t.ID = FIRST_TEAM_ID
+	     JOIN team b ON b.ID = SECOND_TEAM_ID
+	     JOIN stage s ON s.ID = STAGE;
+";
 $result = $connection->query($query);
 if (!$result)
 {
 	trigger_error($connection, E_USER_ERROR);
 }
-
-$countries = [];
-
-while ($row = $result->fetch_assoc())
-{
-	$countries[] = $row;
-}
-
-$row = $result->fetch_all();
-//print_r($row);
-print_r($countries);
--- index.php
+++ index.php
@@ -1,18 +1,37 @@
+<?php
+
+require_once "connect.php";
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/css/styles.css"/>
 	<title>Document</title>
 </head>
 <body>
 	<h1>Tournament</h1>
-	<div class="game">
-		<div class="first-country">
-			p.first-country__
-		</div>
-		<button type="button">Change</button>
-	</div>
+	<?php
+		$games = mysqli_query($connection, $query);
+		$games = mysqli_fetch_all($games);
+		foreach ($games as $game) {
+			echo '
+				<div class="game">
+					<div class="country">
+						<p class="country__name">'.$game[0].'</p>
+						<p class="country__score">'.$game[2].'</p>
+					</div>
+					<div class="country">
+						<p class="country__name">'.$game[1].'</p>
+						<p class="country__score">'.$game[3].'</p>
+					</div>
+					<p class="game__stage">'.$game[4].'</p>
+					<button type="button">Change</button>
+				</div>
+		';}
+	?>
 </body>
 </html>
